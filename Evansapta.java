4cc9081a553a8e49bfdd5bb4462bf6a2d7c8d652
Ramlan Anugrah <fendlodrt969@gmail.com>
2025-12-17 16:56:02 +0700

    menambahkan file

---
+++ Evansapta.java
@@ -0,0 +1,6 @@
+public class Evansapta {
+
+    public static void main(String[] args) {
+        System.out.println("Nama : Evan Sapta Pradana, NIM : 1324068");
+    }
+}
--- epanganteng.java
+++
@@ -1,7 +0,0 @@
-
-public class epanganteng {
-
-    public static void main(String[] args) {
-        System.out.println("Evan Sapta Pradana, NIM 1324068");
-    }
-}
