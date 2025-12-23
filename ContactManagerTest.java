e9c34d06f29571f5aa2d4315335d4f1b15f858bf
irpanstmi <irfansentosa321@gmail.com>
2025-12-24 00:56:02 +0700

    Update ContactManagerTest.java

--- ContactManagerTest.java
+++ ContactManagerTest.java
@@ -12,16 +12,8 @@ public class ContactManagerTest {
 
         javax.swing.SwingUtilities.invokeLater(() -> {
             ContactManagerBackend backend = new ContactManagerBackend();
-
             // Data contoh
-            try {
-                backend.addContact("Evan", "0812345678", backend.getGroups().get(0), true);
-                backend.addContact("Budi", "0898765432", backend.getGroups().get(1), false);
-                backend.addContact("Sari", "0822334455", backend.getGroups().get(2), false);
-                backend.addContact("irpan", "0822334455", backend.getGroups().get(0), false);
-            } catch (ContactManagerBackend.InvalidNumberException e) {
-                e.printStackTrace();
-            }
+
 
             ContactManagerGUI gui = new ContactManagerGUI(backend);
             gui.setVisible(true);
